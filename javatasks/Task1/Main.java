9c6fadd5ba96c5fa4a09b0dce28d407e4c3fd589
Michael Kozhanov <kozhanov312@gmail.com>
2021-02-20 13:39:50 +0300

    gitignore file added

---
+++ javatasks/Task1/Main.java
@@ -0,0 +1,42 @@
+import java.until.Scanner;
+
+
+public class Main
+{
+	// 0
+	/*public static void main(String[] args) {
+		ex1();
+	}*/
+	public static void main(){
+
+		System.out.println("Hello, World");
+	}
+	//1 task
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the number of minutes!");
+		int num
+		in.nextInt();
+		System.out.printf("Number of seconds:", (
+		num * 60));
+		in.close();
+		
+	}
+	//2 task
+	
+	//3 task
+	
+	//4 task
+	
+	//5 task
+	
+	//6 task
+	
+	//7 task
+	
+	//8 task
+	
+	//9 task
+	
+	//10 task
+}
