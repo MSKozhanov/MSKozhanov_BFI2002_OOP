8ba6411e12522c5c1e80a2e25bd18345f5a236d4
Michael Kozhanov <kozhanov312@gmail.com>
2021-02-21 14:19:17 +0300

    Task1 work

--- javatasks/Task1/Main.java
+++ javatasks/Task1/Main.java
@@ -1,21 +1,22 @@
 
-import java.until.Scanner;
+import java.util.Scanner;
 
 public class Main
 {
 	// 0
 	
-	public static void main(String[] args){
+	/*public static void main(String[] args){
 
 		System.out.println("Hello, World");
-	}
+	}*/
 	//1 task
 	public static void main(String[] args){
 
-		Scanner minutes = new Scanner (System.in);
-		int m;
+		Scanner minutes = new Scanner(System.in);
+		int m, res;
 		m = minutes.nextInt();
-		System.out.print("Seconds = " + m * 60);
+		res = m * 60;
+		System.out.print("Seconds = " + res);
 	}
 	//2 task
 	
