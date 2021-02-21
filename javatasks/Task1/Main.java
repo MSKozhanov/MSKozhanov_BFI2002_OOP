60898ac55cc51f23b5b92bf1d0bcedf9f5a4cf12
Michael Kozhanov <kozhanov312@gmail.com>
2021-02-21 14:29:58 +0300

    Task2 the first

--- javatasks/Task1/Main.java
+++ javatasks/Task1/Main.java
@@ -10,16 +10,24 @@ public class Main
 		System.out.println("Hello, World");
 	}*/
 	//1 task
-	public static void main(String[] args){
+	/*public static void main(String[] args){
 
 		Scanner minutes = new Scanner(System.in);
 		int m, res;
 		m = minutes.nextInt();
 		res = m * 60;
 		System.out.print("Seconds = " + res);
-	}
+	}*/
 	//2 task
-	
+	public static void main(String[] args){
+
+		Scanner points = new Scanner(System.in);
+		
+		int p2,p3, res;
+		p2 = points.nextInt();
+		p3 = points.nextInt();
+		res = (p2 * 2) + (p3 * 3);
+		System.out.print("sum = " + res);
 	//3 task
 	
 	//4 task
