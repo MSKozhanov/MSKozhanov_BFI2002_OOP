9dbf346a3202f983be30f24107a73e94622eb7e9
Michael Kozhanov <kozhanov312@gmail.com>
2021-02-21 13:26:07 +0300

    Hello world

--- javatasks/Task1/Main.java
+++ javatasks/Task1/Main.java
@@ -1,27 +1,16 @@
-import java.until.Scanner;
+
 
 
 public class Main
 {
 	// 0
-	/*public static void main(String[] args) {
-		ex1();
-	}*/
-	public static void main(){
+	
+	public static void main(String[] args){
 
 		System.out.println("Hello, World");
 	}
 	//1 task
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.print("Enter the number of minutes!");
-		int num
-		in.nextInt();
-		System.out.printf("Number of seconds:", (
-		num * 60));
-		in.close();
-		
-	}
+	
 	//2 task
 	
 	//3 task
