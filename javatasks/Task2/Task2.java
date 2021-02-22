4b6e36de4f228c47ca3a05ae911e7d223a769fc2
Michael Kozhanov <kozhanov312@gmail.com>
2021-02-22 21:37:59 +0300

    task2

--- javatasks/Task2/Task2.java
+++ javatasks/Task2/Task2.java
@@ -10,17 +10,47 @@ public static int task1(int length, int nomberhouse) {
 	return find;
 			
 }
-public static void main(String[] args){
+/*public static void main(String[] args){
 	Scanner points = new Scanner(System.in);
 	int length, nomberhouse;
 	nomberhouse= points.nextInt();
 	length = points.nextInt();
 	int find = task1(length, nomberhouse); 
 	System.out.println(find);
-}
+}*/
 	
 //task2
+public static String task2(String str) {
 	
+	String res = "";
+	String s1 ="", s2 = "";
+	
+	for(int i = 0; i < str.indexOf(' '); i++) {
+		s1 += str.charAt(i);
+	}
+	
+	for(int i = str.indexOf(' ') + 1; i < str.length(); i++) {
+		s2 += str.charAt(i);
+	}
+	
+	res = s2 + " " + s1;
+	
+	return res;
+	
+}
+
+public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	String str;
+	
+	System.out.println("Enter: ");
+	
+	str = sc.nextLine();
+	
+	System.out.println("Result is: " + task2(str));
+}
 //task3
 	
 //task4
