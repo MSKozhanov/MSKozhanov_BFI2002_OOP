f2945d89c5abb9f4ba542f2a3ab2b919344b066e
Michael Kozhanov <kozhanov312@gmail.com>
2021-03-10 01:36:27 +0300

    task1 complete

--- javatasks/Workspace/Task4_1/src/Task4.java
+++ javatasks/Workspace/Task4_1/src/Task4.java
@@ -1,14 +1,47 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Task4 {
 
 //TASK1
 	
-	public static int task1() {
+	public static String task1(String res) {
+		
+		
+		for(int i = 0; i < res.length(); i++) {
+			if ( res.charAt(i) == '7') {
+				return "Boom";
+			}
+			
+		}
+		return "No";
 		
 	}
 	
 	public static void main(String[] args) {
+		String res = "";
+		String now = "";
 		
+		Scanner sc = new Scanner(System.in);
+		
+		int size;
+		
+		System.out.println("Enter size: ");
+		
+		size = sc.nextInt();
+		
+	    sc.nextLine();
+		
+		System.out.println("Enter element: ");
+		
+		for(int i = 0; i < size; i++) {	
+				now = "";
+				now = sc.nextLine();
+				res += now;
+		}
+		
+		
+		System.out.println("Result is: " + task1(res));
 	}
 //TASK2
 //TASK3
