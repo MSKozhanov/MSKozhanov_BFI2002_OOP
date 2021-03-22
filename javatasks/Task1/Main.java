797ec940d5a0397d4bae3d2818b2ed850d328f5a
Michael Kozhanov <kozhanov312@gmail.com>
2021-03-22 16:40:56 +0300

    task1 restructured

--- javatasks/Task1/Main.java
+++ javatasks/Task1/Main.java
@@ -3,153 +3,267 @@ import java.util.Scanner;
 import javafx.application.Application;
 public class Main
 {
-	// 0
+
+	public static void main(String[] args){
+	task1();
+	task2();
+	task3();
+	task4();
+	task5();
+	task6();
+	task7();
+	task8();
+	task9();
+	task10();
+	}
 	
-	public static void task0(){
-
-		System.out.println("Hello, World");
-	}
 	//1 task
-	public static void task1(String[] args){
-
-		Scanner minutes = new Scanner(System.in);
-		int m, res;
-		m = minutes.nextInt();
-		res = m * 60;
-		System.out.print("Seconds = " + res);
-	}
-	//2 task
-	public static void task2(String[] args){
-
-		//task0();
-		Scanner points = new Scanner(System.in);
+	
+	public static void task1(){
 		
+		System.out.println("Task1_convert_");
+		
+		Scanner minutes = new Scanner(System.in);
+		
+		System.out.print("Enter minutes: ");
+		
+		int m, res;
+		
+		m = minutes.nextInt();
+		
+		res = m * 60;
+		
+		System.out.println("Seconds = " + res);
+	}
+	
+	//2 task
+	
+	public static void task2(){
+
+		System.out.println("Task2_points_");
+		
+		Scanner points = new Scanner(System.in);
 		
 		int p2,p3, res;
+		
+		System.out.print("Enter points 2x: ");
+		
 		p2 = points.nextInt();
+		
+		System.out.print("Enter points 3x: ");
+		
 		p3 = points.nextInt();
+		
 		res = p2 * 2 + p3 * 3;
 		
-		System.out.print("sum = " + res);
+		System.out.println("sum = " + res);
 	}
+	
 	//3 task
 	
-	public static void task3(String[] args){
+	public static void task3(){
 
+		System.out.println("Task3_footballPoints_");
 		
 		Scanner points = new Scanner(System.in);
-		
 		
 		int victory,draw,defeat,res;
+		
+		System.out.print("Enter victory: ");
+		
 		victory = points.nextInt();
+		
+		System.out.print("Enter draw: ");
+		
 		draw = points.nextInt();
+		
+		System.out.print("Enter defeat: ");
+		
 		defeat = points.nextInt();
+		
 		res = victory * 3 + draw * 1 + defeat * 0;
 		
-		System.out.print("sum = " + res);
+		System.out.println("sum = " + res);
 	}
+	
 	//4 task
-	public static void task4(String[] args){
-
+	
+	public static void task4(){
+		
+		System.out.println("Task4_divisibleByFive_");
+		
 		Scanner points = new Scanner(System.in);
+		
 		int digit;
+		
 		boolean division5;
+		
+		System.out.print("Your digit: ");
+		
 		digit = points.nextInt();
 		
-		if(digit % 2 == 0){
+		if(digit % 5 == 0){
 			division5 = true;
 		}else{
 			division5 = false;
 		}
-		System.out.print("Division by 5: " + division5);
+		
+		System.out.println("Division by 5: " + division5);
 	}
+	
 	//5 task
-	public static void task5(String[] args){
-
+	
+	public static void task5(){
+		
+		System.out.println("Task5_and_");
+		
 		Scanner points = new Scanner(System.in);
 		
 		boolean bool1, bool2,res;
+		
+		System.out.print("Enter bool1: ");
+		
 		bool1 = points.nextBoolean();
+		
+		System.out.print("Enter bool2: ");
+		
 		bool2 = points.nextBoolean();
+		
 		if(bool1 == true && bool2 == true){
 			res = true;
 		}
 		else{
 			res = false;
 		}
-		System.out.print("Is true? " + res + "!");
+		
+		System.out.println("Is true? " + res + "!");
 	}
+	
 	//6 task
 	
-	
-	public static void task6(String[] args){
-
+	public static void task6(){
+		
+		System.out.println("Task6_howManyWalls_");
+		
 		Scanner points = new Scanner(System.in);
 		
 		int n, w, h, res;
+		
+		System.out.print("Enter square meters: ");
+		
 		n = points.nextInt();
+		
+		System.out.print("Enter width: ");
+		
 		w = points.nextInt();
+		
+		System.out.print("Enter height: ");
+		
 		h = points.nextInt();
+		
 		res = n / (w * h);
 		
-		System.out.print("How many walls can I paint? " + "You can paint " + res);
-		
+		System.out.println("How many walls can I paint? " + "You can paint " + res);
 	}
+	
 	//7 task
 		
-	public static int squared(int b) {
-		return b * b;
+	public static void task7() {
+		
+		System.out.println("Task7_squared_");
+		
+		Scanner points = new Scanner(System.in);
+		
+		int b, res;
+		
+		System.out.print("Enter digit to make it squared: ");
+		
+		b = points.nextInt();
+		
+		res = b * b;
+		
+		System.out.println("Result is: " + res);
 	}
 
 	//8 task
 
-	public static boolean task8(float prob, int prize, int pay) {
+	public static void task8() {
+		
+		System.out.println("Task8_profitableGamble_");
+		
+		Scanner points = new Scanner(System.in);
+		
+		int prize, pay;
+		
+		float prob;
+		
+		System.out.print("Enter prob: ");
+		
+		prob = points.nextFloat();
+		
+		System.out.print("Enter prize: ");
+		
+		prize = points.nextInt();
+		
+		System.out.print("Enter pay: ");
+		
+		pay = points.nextInt();
+		
+		boolean res;	
 		
 		if (prob * prize > pay){
-			return true;	
+			res = true;	
 		}
 		else{
-			return false;
+			res = false;
 		}
 		
+		System.out.println("Result is: " + res);
 	}
-	/*public static void main(String[] args){ //(prob = 0.2f prize = 50 pay = 9)
-		Scanner points = new Scanner(System.in);
-		int prize, pay;
-		float prob;
-		prob = points.nextFloat();
-		prize = points.nextInt();
-		pay = points.nextInt();
-		boolean profit = task8(prob,prize,pay);
-		System.out.println(profit);
-	}*/
-	//9 task
-	public static int task9(int frame, int minutes){
 	
-		return minutes * 60 * frame;
-	}
-	/*public static void main(String[] args){
-		Scanner points = new Scanner(System.in);
-		int frame, minutes;
-		frame = points.nextInt();
-		minutes = points.nextInt();
-		int frames = task9(frame, minutes); 
-		System.out.println(frames);
+	//9 task
+	
+	public static void task9(){
 		
-	}*/
+		System.out.println("Task9_frames_");
+		
+		Scanner points = new Scanner(System.in);
+		
+		int frame, minutes;
+		
+		System.out.print("Enter frames: ");
+		
+		frame = points.nextInt();
+		
+		System.out.print("Enter minutes: ");
+		
+		minutes = points.nextInt();
+		
+		int res = minutes * 60 * frame;
+		
+		System.out.println("Result is: " + res);
+	}
+	
 	//10 task
 	
-	public static int task10(int digit, int division){
-
-		return (digit - ((digit/division) * division));
-	}
-	
-	public static void main(String[] args){
+	public static void task10(){
+		
+		System.out.println("Task10_mod_");
+		
 		Scanner points = new Scanner(System.in);
+		
 		int digit, division;
+		
+		System.out.print("Enter digit: ");
+		
 		digit = points.nextInt();
+		
+		System.out.print("Enter division digit: ");
+		
 		division = points.nextInt();
-		int res = task10(digit, division); 
-		System.out.println(res);
-	}
+		
+		int res = (digit - ((digit/division) * division)); 
+		
+		System.out.print(res);
+	}	
 }
